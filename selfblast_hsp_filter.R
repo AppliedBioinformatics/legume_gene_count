f975fc8e36cb35c599b716eba9c61b352fd80c02
CassandriaTayFernandez <68932915+CassandriaTayFernandez@users.noreply.github.com>
2022-06-17 14:42:11 +0800

    Create  selfblast_hsp_filter.R

---
+++ selfblast_hsp_filter.R
@@ -0,0 +1,22 @@
+##input: Blast file in outfmt 6 
+usage: python selfblast_hsp_filter.R blast_file.tsv
+
+library(tidyverse)
+library(data.table)
+
+args = commandArgs(trailingOnly=TRUE)
+
+hsp_table <- blasts <- fread(args[1],nThread=20, header = F) %>% as_tibble()
+
+colnames(hsp_table) <- c('qseqid','sseqid','sscinames','scomnames','pident','length','mismatch','gapopen','qstart','qend','sstart','send','ppos','evalue','bitscore')
+
+df = hsp_table %>%
+  group_by(qseqid, sseqid) %>%
+  filter(evalue < 0.01) %>%
+  filter(evalue==min(evalue))
+
+write_tsv (df, paste0(unlist(strsplit(args[1],'[.]'))[1],'_filtered_e001.tsv'))
+nodes = tibble(node=unique(c(df$qseqid,df$sseqid)))
+edges = select(df, qseqid,sseqid)
+write_csv(nodes,paste0(unlist(strsplit(args[1],'[.]'))[1],'_filtered_e001_nodes.csv'))
+write_csv(edges,paste0(unlist(strsplit(args[1],'[.]'))[1],'_filtered_e001_edges.csv'))
